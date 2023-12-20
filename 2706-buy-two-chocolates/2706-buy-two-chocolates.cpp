a2fa795822fdce9761738154f2c4a6a67b317ee2
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2023-12-20 23:32:22 +0530

    Time: 15 ms (45.23%), Space: 46.9 MB (94.77%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int ans = 0; 
+        sort(prices.begin(),prices.end());
+        if(prices[0]+prices[1] <= money){
+            ans = money-(prices[0]+ prices[1]);
+        }else{
+            ans = money;
+        }
+        return ans;
+    }
+};
