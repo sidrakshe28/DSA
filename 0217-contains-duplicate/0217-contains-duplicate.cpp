6940b24c831fe0d66fc381be3961875e355ac8f7
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2023-12-30 00:09:02 +0530

    Time: 109 ms (36.20%), Space: 69.8 MB (67.79%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> seen;
+        for (int num : nums) {
+            if (seen[num] >= 1)
+                return true;
+            seen[num]++;
+        }
+        return false;
+    }
+};
