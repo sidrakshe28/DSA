cf1ad2a18b3640b7b24dfda626336c8b6764ea4a
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2024-01-27 20:29:25 +0530

    Time: 2 ms (72.67%), Space: 14.6 MB (7.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
-void nextPermutation(vector<int>& nums) {
-    next_permutation(nums.begin(), nums.end());
-}
-}; 
+
+    void nextPermutation(vector<int>& nums) {
+        int size = nums.size() ;
+        int index = -1 ;
+        for(int i = size - 2 ; i >= 0 ; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break ;
+            }
+        }
+
+        if(index == -1){
+            reverse(nums.begin(),nums.end());
+           return ;
+        }
+
+        for(int i = size -1 ; i > index ; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i],nums[index]);
+                break ;
+            }
+        }
+        reverse(nums.begin()+index +1,nums.end());
+     
+    }
+};
